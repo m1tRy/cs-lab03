9bd7cf1b594a00358626ba6be24422e754258bb2
m1tRy <m1tRy@mail.com>
2023-06-29 20:28:42 +0300

    code: добавлены модульные тесты

--- lab03/histogram.cpp
+++ lab03/histogram.cpp
@@ -1,14 +1,20 @@
 #include "histogram.h"
 
 void find_minmax(const vector<double>& numbers, double& min, double& max) {
-	min = numbers[0];
-	max = numbers[0];
-	for (double x : numbers) {
-		if (x < min) {
-			min = x;
-		}
-		else if (x > max) {
-			max = x;
+	
+	if (numbers.size() != 0) {
+
+		min = numbers[0];
+		max = numbers[0];
+
+		for (double x : numbers) {
+			if (x < min) {
+				min = x;
+			}
+			else if (x > max) {
+				max = x;
+			}
 		}
 	}
+
 }
